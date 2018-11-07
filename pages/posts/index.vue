a47f437dee959a797f850bf48ce3d7f14a494d68
metal-gogo <daniel@beek.io>
2018-11-07 15:36:42 -0600

    📓 S3 L25 creating main sections (pages)

---
+++ pages/posts/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="posts-page">
+    Some posts will be here
+  </div>
+</template>
+
+<style scoped>
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>

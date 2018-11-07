db532ded0a33fce5fc86f710995723b2f10a4ec3
metal-gogo <daniel@beek.io>
2018-11-07 14:25:48 -0600

    📝 S2 A1 practice pages and routing

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,20 @@
 <template>
   <section class="container">
     <nuxt-link to="/users">Users</nuxt-link>
+    <button @click="onLoadProducts">Products</button>
   </section>
 </template>
+
+<script>
+export default {
+  methods: {
+    onLoadProducts() {
+      this.$router.push('/products');
+    },
+  },
+};
+</script>
+
 
 <style>
 .container {

3b13b714e4eb9883ff2f5a7638440e051aa0098d
metal-gogo <daniel@beek.io>
2018-11-08 12:44:03 -0600

    📓 S3 L32 working on the post page

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      title="Post title 1"
+      preview-text="Preview text 1"
+      thumbnail="http://placekitten.com/200/300"
+    />
+    <PostPreview
+      id="2"
+      title="Post title 2"
+      preview-text="Preview text 2"
+      thumbnail="http://placekitten.com/400/600"
+    />
+    <PostPreview
+      id="3"
+      title="Post title 3"
+      preview-text="Preview text 3"
+      thumbnail="http://placekitten.com/800/1200"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview';
+export default {
+  components: {
+    PostPreview,
+  },
+};
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="posts-page">
-    Some posts will be here
+    <PostList />
   </div>
 </template>
+
+<script>
+import PostList from '@/components/Posts/PostList';
+export default {
+  components: {
+    PostList,
+  },
+};
+</script>
 
 <style scoped>
 .posts-page {

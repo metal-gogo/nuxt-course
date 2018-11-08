e374cdc531c12151fe2714dba4e07ad36a071c29
metal-gogo <daniel@beek.io>
2018-11-08 13:56:29 -0600

    📓 S3 L33 creating an admin section

---
+++ pages/admin/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="admin-page">
+    <section class="new-post">
+      <button @click="$router.push('/admin/new-post')">Create post</button>
+    </section>
+    <section class="existing-posts">
+      <h2>Existing posts</h2>
+      <PostList />
+    </section>
+  </div>
+</template>
+
+<script>
+import PostList from '@/components/Posts/PostList';
+export default {
+  components: {
+    PostList,
+  },
+};
+</script>
+
+<style scoped>
+.admin-page {
+  padding: 20px;
+}
+
+.new-post {
+  text-align: center;
+  border-bottom: 2px solid #ccc;
+  padding-bottom: 10px;
+}
+
+.existing-posts h2 {
+  text-align: center;
+}
+</style>
